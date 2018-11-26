edcc2f538edbb0348aea2eed9a7de159af46c896
tragetraje <z.a.astafyeva@gmail.com>
2018-11-26 23:20:22 +1100

    add solution for exercise 1

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,5 +5,17 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
     public static void main(String[] args) {
 
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 2 != 0) {
+                System.out.println(i);
+                sum = sum + i;
+            }
+        }
+
+        System.out.println("The sum of odd numbers from 1 to 100 is " + sum);
+
     }
 }
