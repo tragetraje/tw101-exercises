b1f96c6ee8ed0c284211bdb9540dfa61e172340d
tragetraje <z.a.astafyeva@gmail.com>
2018-12-18 22:29:54 +1100

    submit another exercise solution

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,8 +1,24 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
+    private int sum;
 
+    public OddSum() {
+        sum = 0;
+    }
     public int of(int start, int end) {
-        return -1;
+
+        for (int i = start; i <= end; i++) {
+
+            if (isOdd(i)) {
+                System.out.println(i);
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+
+    private boolean isOdd(int number) {
+        return number % 2 != 0;
     }
 }
