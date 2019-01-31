fc973fd71ed79b5c33dd64c137077d7aa621902d
tragetraje <z.a.astafyeva@gmail.com>
2019-01-31 22:50:28 +1100

    submit solution for 4th exercise

--- src/main/java/com/thoughtworks/tw101/exercises/exercise4/Library.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise4/Library.java
@@ -12,5 +12,30 @@ public class Library {
     }
 
     public void printBooksContaining(String partialBookTitle) {
+
+        for (int i = 0; i < this.books.length; i++) {
+            String[] bookNameAsArray = this.splitName(this.books[i]);
+
+            if (this.hasPartialBookTitle(bookNameAsArray, partialBookTitle)) {
+                System.out.println("Books containing word " + partialBookTitle + ':');
+                this.printStream.print(this.books[i]);
+                System.out.println();
+            }
+        }
+    }
+
+    public String[] splitName(String bookName) {
+        return bookName.split("\\s+");
+    }
+
+    public boolean hasPartialBookTitle(String[] bookTitle, String partial) {
+
+        for (int j=0; j < bookTitle.length; j++) {
+
+            if (bookTitle[j].equals(partial)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
